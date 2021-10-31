932e7785ba9f2bb5c788413d83658a02ee88ba8d
Johny-Wood <craftarttv@gmail.com>
2021-10-31 20:28:35 +0300

    check fix

--- calc.js
+++ calc.js
@@ -1,21 +1,21 @@
 function Calc(operation, Num1, Num2) {
-	let opList = "add | multi | divide | subtract | divide | exp";
+  let opList = ['add', 'multi', 'divide', 'subtract', 'divide', 'exp'];
   
-	if (operation == 'add') {
-  	return Num1 + Num2
+  if (!operation || Num1 === undefined || !Num2) {
+    console.log('Error');
+ }  else if (!opList.includes(operation)) {
+      console.log("Unknown operation! Try some of these: " + opList);
+  } else if (operation == 'add') {
+      return Num1 + Num2;
   } else if (operation == 'subtract') {
-  	return Num1 - Num2
+      return Num1 - Num2;
   } else if (operation == 'multi') {
-  	return Num1 * Num2
+      return Num1 * Num2;
   } else if (operation == 'divide') {
-  	return Num1 / Num2
+      return Num1 / Num2;
   } else if (operation == 'exp') {
-  	return Num1 ** Num2
-  } else if ((Num1 == "") || (Num2 == "") || (typeof Num1 != Number)) {
-  	console.log('Error');
-  } else if (operation == "") {
-  	console.log("Unknown operation! Try some of these:" + opList)
+      return Num1 ** Num2;
   }
 };
 
-console.log(Calc('exp', 4, 2));
+console.log(Calc('add', 4, 2));
