e64a7d699d99174b9f8a3fd733b328384256c11a
Johny-Wood <craftarttv@gmail.com>
2021-11-10 14:07:46 +0300

    Added verticalShow

---
+++ verticalShow.js
@@ -0,0 +1,11 @@
+function showVerticalMessage(string) {
+
+	string = string.slice(0, 10);
+  
+ 	for (let char of string) {
+  		console.log(char)
+  	}
+  	
+}
+
+showVerticalMessage('Марафондлядрузей')
